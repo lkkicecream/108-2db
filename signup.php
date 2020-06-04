3e65d6e83bee2eafbbdf4076ee62520d6d935372
lkkicecream <47779334+lkkicecream@users.noreply.github.com>
2020-06-04 20:51:42 +0800

    Add files via upload

--- signup.php
+++ signup.php
@@ -61,7 +61,7 @@
     }
 
     body {
-      background-color: #d9b68b;
+      background-color: white;
     }
 
     .card-new .card {
@@ -114,5 +114,45 @@
       font-size: medium;
       text-align: center;
     }
+    .main{
+				margin: 0 auto;
+				padding: 10px;
+				width: 350px;
+				height: 300px;
+				background: white;
+			}
+			.leftbar{
+				width: 30%;
+				padding-bottom: 10px;
+				display: inline-block;
+				text-align: right;
+			}
+			.bottom{
+				padding-bottom: 15px;
+			}
   </style>
 </head>
+<body>
+		
+		<form action="login.php" method="post">
+			
+			<div id="main" class="main">
+				<h3>
+					請輸入使用者名稱
+				</h3>
+				<div>
+					<label><div class="news">使用者名稱：</div><input type="text" name="userName" /></label>
+					<label><div class="news">密碼：</div><input type="text" name="userPassword" /></label>
+				</div>
+				<div class="bottom">
+					<div class="news"></div><input type="radio" name="remmber"  />記住我一週
+				</div>
+				<div class="bottom">
+					<div class="leftbar"></div><input type="submit" name="submit" value="登入" />
+				</div>
+				
+			</div>
+			
+		</form>	
+  </body>
+  </html>
