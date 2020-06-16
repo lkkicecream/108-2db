b67960f7a4785b072c9209a46b1658b75c5beec5
jack1220 <jason881220@gmail.com>
2020-06-16 21:41:43 +0800

    刪掉測試用的多餘東西

--- personal.php
+++ personal.php
@@ -153,27 +153,7 @@
   </script>
 </head>
 
-<body>
-<?php
-  echo "
-    <nav class=\"navbar navbar-expand-lg navbar-light\" style= \"margin-top: 0px; margin-left: 1200px\">
-    <div class=\"collapse navbar-collapse\" id=\"navbarNavDropdown\">
-    <ul class=\"navbar-nav\">
-    <li class=\"nav-item dropdown\">
-                    <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdownMenuLink\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
-                        Dropdown link
-                    </a>
-                    <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdownMenuLink\">
-                        <a class=\"dropdown-item\" href=\"#\">Action</a>
-                        <a class=\"dropdown-item\" href=\"#\">Another action</a>
-                        <a class=\"dropdown-item\" href=\"#\">Something else here</a>
-                    </div>
-                </li>
-                </ul>
-        </div>
-    </nav>
-    ";
-  ?>    
+<body>   
   <!-- Header -->
   <header id="header-fixed" class="header-fixed-open">
     <!-- Logo -->
